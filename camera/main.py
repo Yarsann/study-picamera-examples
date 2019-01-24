2489de3f70f469cc08ed013b1ae8314d28da37b1
Yarsann <46982451+Yarsann@users.noreply.github.com>
2019-01-24 22:37:14 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Response
-# from processor.simple_streamer import SimpleStreamer as VideoCamera
+from processor.simple_streamer import SimpleStreamer as VideoCamera
 # from processor.pedestrian_detector import PedestrianDetector as VideoCamera
 # from processor.motion_detector import MotionDetector as VideoCamera
 # from processor.qr_detector import QRDetector as VideoCamera
@@ -10,6 +10,8 @@ import time
 import threading
 
 video_camera = VideoCamera(flip=False)
+
+
 app = Flask(__name__)
 
 @app.route('/')
